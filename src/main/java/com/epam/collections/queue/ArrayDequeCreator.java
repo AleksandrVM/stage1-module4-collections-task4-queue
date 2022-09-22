67ae1e1e38d1bebaa1e54da7101f2b740808bbb8
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-09-22 17:20:43 +0300

    Solution stage#1. module#4. Queue Interface Tasks.

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,28 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.addLast(firstQueue.poll());
+        arrayDeque.addLast(firstQueue.poll());
+        arrayDeque.addLast(secondQueue.poll());
+        arrayDeque.addLast(secondQueue.poll());
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            playerMove(arrayDeque, firstQueue);
+            playerMove(arrayDeque, secondQueue);
+        }
+        return arrayDeque;
+    }
+
+    private void playerMove(Deque<Integer> deck, Queue<Integer> hand){
+        hand.add(deck.pollLast());
+        deck.offerLast(hand.poll());
+        deck.offerLast(hand.poll());
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> queue = new PriorityQueue<>(new Comparator<>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return -1 * s1.compareTo(s2);
+            }
+        });
+        queue.addAll(firstList);
+        queue.addAll(secondList);
+        return queue;
     }
 }
